6c0dfe51da3e9ab72d57b10260fa4c785caef9bf
dislersd <dislersd@gmail.com>
2019-02-22 10:21:32 -0800

    refactored top bar styling to styled components

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,5 +1,5 @@
 import React from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import Tab from "./Tab";
 
 const Tabs = props => {
@@ -7,9 +7,6 @@ const Tabs = props => {
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
-        {/* map over the tabs provided on your props, create a new Tab component for each one.
-            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
-
         {props.tabs.map(tab => (
           <Tab
             tab={tab}
